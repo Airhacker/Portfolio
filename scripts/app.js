72636768e54ddc4dee6b4186b6fddd8f93b704e5
Umair Ahmed <umairahmed.dev@gmail.com>
2020-04-15 21:38:57 -0700

    swipable pages

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,18 @@
-// Three JS Implementation
-const scene = new THREE.Scene();
-
-// Camera Properties
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(0, 1, 2);
-
-//Lights
-
-// 3D Object
-let geometry = new THREE.CylinderGeometry(1, 1, 1, 3);
-let material = new THREE.MeshBasicMaterial({
-  color: 0x102a43,
-  transparent: false,
-  blending: THREE.AdditiveBlending,
+let swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  loop: true,
+  grabCursor: false,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
 });
-let cylinder = new THREE.Mesh(geometry, material);
-scene.add(cylinder);
-
-//grid
-var gridHelper = new THREE.GridHelper(10, 20);
-scene.add(gridHelper);
-
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-let animate = function () {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-};
-
-animate();
