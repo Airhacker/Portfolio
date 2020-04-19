669989fd3ae5d7a0571b7f4ded1a8d689ca3a254
Umair Ahmed <umairahmed.dev@gmail.com>
2020-04-19 02:24:35 -0700

    modal initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,36 @@
-// Page swipe
-let swiper1 = new Swiper(".swiper1", {
-  effect: "coverflow",
-  loop: true,
-  grabCursor: false,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
+// Project Modal
+const projectContainer = document.querySelector("#projects-container");
+
+projectContainer.addEventListener("click", (event) => {
+	target = event.target;
+
+	switch (target.id) {
+		case "aamir":
+			console.log("Aamir Here");
+			break;
+	}
 });
 
-// Project Swipe
-let swiper2 = new Swiper(".swiper2", {
-  grabCursor: true,
-  freeMode: true,
-  slidesPerView: "3",
-  direction: "vertical",
-  spaceBetween: 10,
-  mouseWheel: true,
-  loop: true,
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+// Modal Creator
+
+// Page swipe
+let swiper1 = new Swiper(".swiper1", {
+	effect: "coverflow",
+	loop: true,
+	grabCursor: false,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
+		clickable: true,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		dynamicBullets: true,
+		clickable: true,
+	},
 });
